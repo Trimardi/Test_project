c071cf9e1b376033660d71c21f9a9916447ed72c
Trimardi <trimardi37@outlook.com>
2016-12-24 16:38:25 -0400

    revisado 1.2
    Signed-off-by: Trimardi <trimardi37@outlook.com>

--- ionic/js/controllers.js
+++ ionic/js/controllers.js
@@ -74,7 +74,7 @@ angular.module('conFusion.controllers', [])
 
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL' ,function($scope, menuFatory, baseURL){
+.controller('MenuController', ['$scope', 'menuFactory', 'baseURL' , 'favoriteFactory', '$ionicListDelegate' ,function($scope, menuFatory, baseURL, favoriteFactory, $ionicListDelegate){
 
   $scope.baseURL = baseURL;
   $scope.tab = 1;
@@ -117,6 +117,12 @@ angular.module('conFusion.controllers', [])
   $scope.toggleDetails = function(){
     $scope.showDetails = !$scope.showDetails;
   };
+
+  $scope.addFavorite = function(index){
+    console.log("index is " + index);
+    favoriteFactory.addToFavorites(index);
+    $ionicListDelegate.closeOptionButtons();
+  }
 
 }])
 
@@ -222,5 +228,45 @@ angular.module('conFusion.controllers', [])
   console.log($scope.leaders);
 
 }])
+
+.controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+
+    $scope.baseURL = baseURL;
+    $scope.shouldShowDelete = false;
+
+    $scope.favorites = favoriteFactory.getFavorites();
+
+    $scope.dishes = menuFactory.getDishes().query(
+        function (response) {
+            $scope.dishes = response;
+        },
+        function (response) {
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+        });
+    console.log($scope.dishes, $scope.favorites);
+
+    $scope.toggleDelete = function () {
+        $scope.shouldShowDelete = !$scope.shouldShowDelete;
+        console.log($scope.shouldShowDelete);
+    }
+
+    $scope.deleteFavorite = function (index) {
+        
+        favoriteFactory.deleteFromFavorites(index);
+        $scope.shouldShowDelete = false;
+
+    }}])
+
+  .filter('favoriteFilter', function () {
+    return function (dishes, favorites) {
+        var out = [];
+        for (var i = 0; i < favorites.length; i++) {
+            for (var j = 0; j < dishes.length; j++) {
+                if (dishes[j].id === favorites[i].id)
+                    out.push(dishes[j]);
+            }
+        }
+        return out;
+    }})
 
 ;
